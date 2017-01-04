eae0a8185b5a7712831e3402e31ccca46819ecc4
Steven Testa <steve@testasoftware.com>
2017-01-04 15:47:51 -0500

    updated tests and began alu implementation

--- test/test-alu.js
+++ test/test-alu.js
@@ -1,4 +1,27 @@
 'use strict';
 
 var assert = require('assert');
-//var alu = require('../lib/alu');
+var alu = require('../lib/alu');
+
+describe('alu', function() {
+  it('x', function() {
+    //                  |- inputs  -| |- xs  -|  |- ys  -|
+    assert.deepEqual(alu(0,0,1,1,0,0, [0,0,1,1], [0,1,0,1]),
+                    [0,0,1,1]);
+  });
+  it('y', function() {
+    //                  |- inputs  -| |- xs  -|  |- ys  -|
+    assert.deepEqual(alu(1,1,0,0,0,0, [0,0,1,1], [0,1,0,1]),
+                    [0,1,0,1]);
+  });
+  it('!x', function() {
+    //                  |- inputs  -| |- xs  -|  |- ys  -|
+    assert.deepEqual(alu(0,0,1,1,0,1, [0,0,1,1], [0,1,0,1]),
+                    [1,1,0,0]);
+  });
+  it('!y', function() {
+    //                  |- inputs  -| |- xs  -|  |- ys  -|
+    assert.deepEqual(alu(1,1,0,0,0,1, [0,0,1,1], [0,1,0,1]),
+                    [1,0,1,0]);
+  });
+});
