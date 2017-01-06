f4e37052b6eaa8520f15b8d8d91f2f39f788d6df
Steven Testa <steve@testasoftware.com>
2017-01-05 20:57:00 -0500

    minor test bugfix; update to travis-ci config

--- test/test-alu.js
+++ test/test-alu.js
@@ -4,24 +4,16 @@ var assert = require('assert');
 var alu = require('../lib/alu');
 
 describe('alu', function() {
-  it('x', function() {
-    //                  |- inputs  -| |- xs  -|  |- ys  -|
-    assert.deepEqual(alu(0,0,1,1,0,0, [0,0,1,1], [0,1,0,1]),
-                    [0,0,1,1]);
+  it('zx:0, nx:0, zy:1, ny:1, f:0, no:0 (x)', function() {
+    assert.deepEqual(alu(0,0,1,1,0,0, [0,0,1,1], [0,1,0,1]), [0,0,1,1]);
   });
-  it('y', function() {
-    //                  |- inputs  -| |- xs  -|  |- ys  -|
-    assert.deepEqual(alu(1,1,0,0,0,0, [0,0,1,1], [0,1,0,1]),
-                    [0,1,0,1]);
+  it('zx:1, nx:1, zy:0, ny:0, f:0, no:0 (y)', function() {
+    assert.deepEqual(alu(1,1,0,0,0,0, [0,0,1,1], [0,1,0,1]), [0,1,0,1]);
   });
-  it('!x', function() {
-    //                  |- inputs  -| |- xs  -|  |- ys  -|
-    assert.deepEqual(alu(0,0,1,1,0,1, [0,0,1,1], [0,1,0,1]),
-                    [1,1,0,0]);
+  it('zx:0, nx:0, zy:1, ny:1, f:0, no:1 (!x)', function() {
+    assert.deepEqual(alu(0,0,1,1,0,1, [0,0,1,1], [0,1,0,1]), [1,1,0,0]);
   });
-  it('!y', function() {
-    //                  |- inputs  -| |- xs  -|  |- ys  -|
-    assert.deepEqual(alu(1,1,0,0,0,1, [0,0,1,1], [0,1,0,1]),
-                    [1,0,1,0]);
+  it('zx:1, nx:1, zy:0, ny:0, f:0, no:1 (!y)', function() {
+    assert.deepEqual(alu(1,1,0,0,0,1, [0,0,1,1], [0,1,0,1]), [1,0,1,0]);
   });
 });
